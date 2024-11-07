43425acece1453e17a937667c952bace96ebfab1
Vanshraj Surendra Chauhan <8460630314m@gmail.com>
2024-11-07 08:55:41 +0530

    Add files via upload

---
+++ A Paint Eraser/eraser.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(1500, 600);
+     background(0);
+  }
+  
+  function draw() {
+   
+    ellipse(mouseX,mouseY,50,50)
+    noStroke()
+  }
+
+  function mousePressed() {
+    background(0)
+  }
