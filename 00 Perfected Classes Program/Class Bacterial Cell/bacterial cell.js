110d763bf4d78ad1eddd88ef3c82bb0f3f3108de
Vanshraj Surendra Chauhan <8460630314m@gmail.com>
2024-12-28 17:29:41 +0530

    bacterial cell.js

--- 00 Perfected Classes Program/Class Bacterial Cell/bacterial cell.js
+++ 00 Perfected Classes Program/Class Bacterial Cell/bacterial cell.js
@@ -1,19 +1,22 @@
+// A program where bubbles are created on mouse click, move randomly, and remain on the screen.
+
 let bubble = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 }
 
-function mousePressed(){
-    let r = random(20, 40);
-    let b = new Bubble(mouseX, mouseY, r)
-    bubble.push(b);
+function mousePressed() {
+  // Create a new bubble at the mouse position with a random radius
+  let r = random(20, 40);
+  let b = new Bubble(mouseX, mouseY, r);
+  bubble.push(b);
 }
 
-
 function draw() {
+  // Clear the background and display all bubbles
   background(0);
-  for (i=0; i< bubble.length; i++) {
+  for (i = 0; i < bubble.length; i++) {
     bubble[i].move();
     bubble[i].show();
   }
@@ -21,18 +24,21 @@ function draw() {
 
 class Bubble {
   constructor(x, y, r) {
+    // Initialize bubble with position and radius
     this.x = x;
     this.y = y;
     this.r = r;
   }
 
   move() {
+    // Move the bubble randomly within a small range
     this.x += random(-2, 2);
     this.y += random(-2, 2);
   }
 
   show() {
-    stroke(0, 150, 255, 150); 
+    // Draw the bubble with a blue stroke and no fill
+    stroke(0, 150, 255, 150);
     strokeWeight(2);
     fill(0);
     ellipse(this.x, this.y, this.r * 2);
