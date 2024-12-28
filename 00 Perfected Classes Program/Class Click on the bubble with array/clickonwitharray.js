ecc8c9267ca4de118886e09915dad2152e27958f
Vanshraj Surendra Chauhan <8460630314m@gmail.com>
2024-12-28 17:35:17 +0530

    clickonwitharray.js

--- 00 Perfected Classes Program/Class Click on the bubble with array/clickonwitharray.js
+++ 00 Perfected Classes Program/Class Click on the bubble with array/clickonwitharray.js
@@ -1,51 +1,59 @@
-let bubble=[];
+// A program where bubbles move randomly on the canvas, and a message is logged to the console when a bubble is clicked.
+
+let bubble = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for(i=0;i<5;i++){
-  let x = random(width);
-  let y = random(height);
-  let r = random(20, 50);
-  let b = new Bubble(x, y, r);
-  bubble.push(b)
-}}
+  // Create 5 bubbles with random positions and sizes
+  for (let i = 0; i < 5; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(20, 50);
+    let b = new Bubble(x, y, r);
+    bubble.push(b);
+  }
+}
 
 function mousePressed() {
-  for(i=0;i< bubble.length;i++){
-  bubble[i].clicked(mouseX,mouseY);
-}}
-  
-
+  // Check if a bubble is clicked
+  for (let i = 0; i < bubble.length; i++) {
+    bubble[i].clicked(mouseX, mouseY);
+  }
+}
 
 function draw() {
   background(0);
-  for(i=0;i< bubble.length;i++){
-  bubble[i].move();
-  bubble[i].show();
-
-}}
+  // Show and move all bubbles
+  for (let i = 0; i < bubble.length; i++) {
+    bubble[i].move();
+    bubble[i].show();
+  }
+}
 
 class Bubble {
   constructor(x, y, r) {
+    // Initialize bubble position and radius
     this.x = x;
     this.y = y;
     this.r = r;
   }
 
-  clicked(mx,my) {
+  clicked(mx, my) {
+    // Check if the mouse is within the bubble's radius
     let d = dist(mx, my, this.x, this.y);
     if (d < this.r) {
       console.log("Clicked on the bubble");
-      
     }
   }
 
   move() {
+    // Move the bubble randomly
     this.x += random(-2, 2);
     this.y += random(-2, 2);
   }
 
   show() {
+    // Draw the bubble
     stroke(0, 150, 255, 150);
     strokeWeight(2);
     fill(0);
