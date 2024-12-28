acce0cb3ebd112d1b854a42b369e5e6b3e3520cb
Vanshraj Surendra Chauhan <8460630314m@gmail.com>
2024-12-28 17:38:33 +0530

    clicked_with_button.js

--- 00 Perfected Classes Program/Class Clicked on the bubble with button/clicked_with_button.js
+++ 00 Perfected Classes Program/Class Clicked on the bubble with button/clicked_with_button.js
@@ -1,55 +1,64 @@
-let bubble=[];
+// A program where bubbles move randomly and change brightness when clicked, cycling through a range from 0 to 255.
+
+let bubble = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for(i=0;i<5;i++){
-  let x = random(width);
-  let y = random(height);
-  let r = random(20, 50);
-  let b = new Bubble(x, y, r);
-  bubble.push(b)
-}}
+  // Create 5 bubbles with random positions and sizes
+  for (let i = 0; i < 5; i++) {
+    let x = random(width);
+    let y = random(height);
+    let r = random(20, 50);
+    let b = new Bubble(x, y, r);
+    bubble.push(b);
+  }
+}
 
 function mousePressed() {
-  for(i=0;i< bubble.length;i++){
-  bubble[i].clicked(mouseX,mouseY);
-}}
-  
-
+  // Check if a bubble is clicked
+  for (let i = 0; i < bubble.length; i++) {
+    bubble[i].clicked(mouseX, mouseY);
+  }
+}
 
 function draw() {
   background(0);
-  for(i=0;i< bubble.length;i++){
-  bubble[i].move();
-  bubble[i].show();
-
-}}
+  // Show and move all bubbles
+  for (let i = 0; i < bubble.length; i++) {
+    bubble[i].move();
+    bubble[i].show();
+  }
+}
 
 class Bubble {
   constructor(x, y, r) {
+    // Initialize bubble position, radius, and brightness
     this.x = x;
     this.y = y;
     this.r = r;
-    this.brightness=0;
+    this.brightness = 0;
   }
 
-  clicked(mx,my) {
+  clicked(mx, my) {
+    // Check if the mouse is within the bubble's radius
     let d = dist(mx, my, this.x, this.y);
     if (d < this.r) {
-      //console.log("Clicked on the bubble");
-      this.brightness+=250
-      if(this.brightness>255){
-        this.brightness=0;
-      }   
+      // Change the brightness when clicked
+      this.brightness += 250;
+      if (this.brightness > 255) {
+        this.brightness = 0; // Reset if it exceeds 255
+      }
     }
   }
 
   move() {
+    // Move the bubble randomly
     this.x += random(-2, 2);
     this.y += random(-2, 2);
   }
 
   show() {
+    // Draw the bubble with the current brightness
     stroke(0, 150, 255, 150);
     strokeWeight(2);
     fill(this.brightness);
